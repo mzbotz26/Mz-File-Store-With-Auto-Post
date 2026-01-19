fb2be16bd4cc0d416b3b09fe3ba84fa0820263a9
mzbotz26 <mrsam2395@gmail.com>
2026-01-19 21:45:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -83,3 +83,6 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
+
+if __name__ == "__main__":
+    Bot().run()
